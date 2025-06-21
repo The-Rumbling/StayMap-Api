3039f1901bdd9ee6b783f271cdfe625a967182d7
Antuanete01 <u202310358@upc.edu.pe>
2025-06-20 19:26:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,9 +9,10 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
+// Leer base de datos
 let db = JSON.parse(fs.readFileSync('db.json', 'utf8'));
 
-// Ruta especial para buscar usuario por email
+// Buscar usuario por email
 app.get('/users', (req, res) => {
   const { email } = req.query;
   if (email) {
@@ -22,20 +23,7 @@ app.get('/users', (req, res) => {
   }
 });
 
-// CRUD genérico para cada colección
-Object.keys(db).forEach((key) => {
-  // GET ALL (excepto users, ya manejado arriba)
-  if (key !== 'users') {
-    app.get(`/${key}`, (req, res) => res.json(db[key]));
-  }
-
-  // GET BY ID
-  app.get(`/${key}/:id`, (req, res) => {
-    const item = db[key].find(i => i.id == req.params.id);
-    item ? res.json(item) : res.sendStatus(404);
-  });
-
-// GET posts by communityId
+// Obtener posts por communityId
 app.get('/posts', (req, res) => {
   const { communityId } = req.query;
   if (communityId) {
@@ -45,7 +33,23 @@ app.get('/posts', (req, res) => {
     res.json(db.posts);
   }
 });
-  
+
+// CRUD general para cada colección (menos users y posts)
+Object.keys(db).forEach((key) => {
+  if (key !== 'users' && key !== 'posts') {
+    // GET ALL
+    app.get(`/${key}`, (req, res) => {
+      res.json(db[key]);
+    });
+  }
+
+  // GET BY ID
+  app.get(`/${key}/:id`, (req, res) => {
+    const collection = db[key];
+    const item = collection.find(i => i.id == req.params.id);
+    item ? res.json(item) : res.sendStatus(404);
+  });
+
   // POST
   app.post(`/${key}`, (req, res) => {
     const nuevo = req.body;
@@ -60,7 +64,9 @@ app.get('/posts', (req, res) => {
     if (index !== -1) {
       db[key][index] = { ...req.body, id: req.params.id };
       res.json(db[key][index]);
-    } else res.sendStatus(404);
+    } else {
+      res.sendStatus(404);
+    }
   });
 
   // DELETE
@@ -69,7 +75,9 @@ app.get('/posts', (req, res) => {
     if (index !== -1) {
       db[key].splice(index, 1);
       res.sendStatus(204);
-    } else res.sendStatus(404);
+    } else {
+      res.sendStatus(404);
+    }
   });
 });
 
